996703f436a5d1001d412e311e32c061a45d69f4
Najmun Nahar Neha <najmun.nahar.nehaa@email.com>
2025-10-10 13:06:56 +0600

    add: TextInput Component

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useForm } from "@inertiajs/vue3";
+import TextInput from "../components/TextInput.vue";
 
 const form = useForm({
     name: null,
@@ -21,25 +22,29 @@ const submit = () => {
     <h1 class="title">Register a new account</h1>
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="submit">
-            <div class="mb-6">
-                <label>Name</label>
-                <input type="text" v-model="form.name" />
-                <small>{{ form.errors.name }}</small>
-            </div>
-            <div class="mb-6">
-                <label>Email</label>
-                <input type="text" v-model="form.email" />
-                <small>{{ form.errors.email }}</small>
-            </div>
-            <div class="mb-6">
-                <label>Password</label>
-                <input type="password" v-model="form.password" />
-                <small>{{ form.errors.password }}</small>
-            </div>
-            <div class="mb-6">
-                <label>Confirm Password</label>
-                <input type="password" v-model="form.password_confirmation" />
-            </div>
+            <TextInput
+                name="name"
+                v-model="form.name"
+                :message="form.errors.name"
+            />
+            <TextInput
+                name="email"
+                type="email"
+                v-model="form.email"
+                :message="form.errors.email"
+            />
+            <TextInput
+                name="password"
+                type="password"
+                v-model="form.password"
+                :message="form.errors.password"
+            />
+            <TextInput
+                name="confirm password"
+                type="password"
+                v-model="form.password_confirmation"
+            />
+
             <div>
                 <p class="text-slate-600 mb-2">
                     Already a user?
